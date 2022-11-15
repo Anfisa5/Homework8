1292843292048d86a693cfd4217818f7bd2a8b95
Anfisa Golovanova <anfisa.faa@gmail.com>
2022-11-16 02:59:41 +0700

    add file and file gitignore

---
+++ src/main/java/ru/netology/FormDate.java
@@ -0,0 +1,9 @@
+package ru.netology;
+
+public class FormDate {
+
+    public int day; // поле для числа рождения
+    public int month; // поле для месяца рождения
+    public int year; // поле для года рождения
+
+}
---
+++ src/main/java/ru/netology/Post.java
@@ -0,0 +1,13 @@
+package ru.netology;
+
+public class Post {
+    public String name; // поле для имени
+    public String passport; // поле для номера паспорта
+    public String patronymic; // поле для отчества
+    public String phone; // поле для номера телефона
+    public String surname; // поле для фамилии
+    public boolean subscription; // поле пользователь получать новости о скидках в виде sms
+    public FormDate birthday; // поле для даты рождения
+
+}
+
